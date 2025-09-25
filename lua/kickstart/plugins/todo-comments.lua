a5fb996b1d8a2fda588518736f8a4520ff41965b
Truls Borgvall <truls.borgvall@icloud.com>
2025-09-25 12:25:43 +0200

    refactor: move plugin initializations to seperate files

---
+++ lua/kickstart/plugins/todo-comments.lua
@@ -0,0 +1,9 @@
+-- Highlight todo, notes, etc in comments
+-- https://github.com/folke/todo-comments.nvim
+
+return {
+  'folke/todo-comments.nvim',
+  event = 'VimEnter',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = { signs = false },
+}
