8087a0424e1a947b87c6120575a4eea44053641e
Truls Borgvall <truls.borgvall@icloud.com>
2025-09-26 16:32:49 +0200

    refactor: remove some comments

--- init.lua
+++ init.lua
@@ -39,12 +39,8 @@ require('lazy').setup({
   -- Import all plugins from kickstart/plugins directory
   { import = 'kickstart.plugins' },
 
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   { import = 'custom.plugins' },
-  --
+ 
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
