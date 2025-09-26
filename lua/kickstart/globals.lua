ad2591c3377a9fe660da1e0e1192a5e52c092141
Truls Borgvall <truls.borgvall@icloud.com>
2025-09-26 16:09:44 +0200

    feat: enable nerd font

--- lua/kickstart/globals.lua
+++ lua/kickstart/globals.lua
@@ -7,4 +7,4 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
