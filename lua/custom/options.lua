75a65ac930ce0afb727af0862929050b1929094d
Truls Borgvall <truls.borgvall@icloud.com>
2025-09-28 20:10:07 +0200

    feat: add tab configuration

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -3,3 +3,11 @@
 
 -- Auto-reload files changed outside of Neovim
 vim.o.autoread = true
+
+-- Use spaces instead of tabs
+vim.opt.expandtab = true
+
+-- Use 2 spaces per tab
+vim.opt.shiftwidth = 2 -- spaces per indent (Prettier default)
+vim.opt.tabstop = 2 -- how many spaces a <Tab> counts for
+vim.opt.softtabstop = 2 -- how many spaces inserted when you press <Tab>
