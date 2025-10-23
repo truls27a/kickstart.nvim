121572f2a83280cbf100ac5458790c432432e505
Truls Borgvall <truls.borgvall@icloud.com>
2025-10-23 13:00:40 +0200

    feat: simplify neo tree configuration

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -2,138 +2,17 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  branch = 'v3.x',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons',
-    'MunifTanjim/nui.nvim',
-  },
-  keys = {
-    { '<leader>e', '<cmd>Neotree toggle<cr>', desc = 'Toggle Neo-tree' },
-  },
-  opts = {
-    close_if_last_window = true,
-    popup_border_style = 'rounded',
-    enable_git_status = true,
-    enable_diagnostics = true,
-    open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' },
-    default_component_configs = {
-      indent = {
-        indent_size = 2,
-        padding = 1,
-        with_markers = true,
-        indent_marker = '│',
-        last_indent_marker = '└',
-        highlight = 'NeoTreeIndentMarker',
-      },
-      icon = {
-        folder_closed = '',
-        folder_open = '',
-        folder_empty = '',
-        default = '*',
-        highlight = 'NeoTreeFileIcon',
-      },
-      git_status = {
-        symbols = {
-          added = '✚',
-          modified = '',
-          deleted = '✖',
-          renamed = '',
-          untracked = '',
-          ignored = '',
-          unstaged = '',
-          staged = '',
-          conflict = '',
-        },
-      },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      'nvim-tree/nvim-web-devicons', -- optional, but recommended
     },
-    window = {
-      position = 'left',
-      width = 35,
-      mapping_options = {
-        noremap = true,
-        nowait = true,
-      },
-      mappings = {
-        ['<space>'] = {
-          'toggle_node',
-          nowait = false,
-        },
-        ['<2-LeftMouse>'] = 'open',
-        ['<cr>'] = 'open',
-        ['<esc>'] = 'cancel',
-        ['P'] = { 'toggle_preview', config = { use_float = true, use_image_nvim = true } },
-        ['l'] = 'focus_preview',
-        ['S'] = 'open_split',
-        ['s'] = 'open_vsplit',
-        ['t'] = 'open_tabnew',
-        ['C'] = 'close_node',
-        ['z'] = 'close_all_nodes',
-        ['a'] = {
-          'add',
-          config = {
-            show_path = 'none',
-          },
-        },
-        ['A'] = 'add_directory',
-        ['d'] = 'delete',
-        ['r'] = 'rename',
-        ['y'] = 'copy_to_clipboard',
-        ['x'] = 'cut_to_clipboard',
-        ['p'] = 'paste_from_clipboard',
-        ['c'] = 'copy',
-        ['m'] = 'move',
-        ['q'] = 'close_window',
-        ['R'] = 'refresh',
-        ['?'] = 'show_help',
-        ['<'] = 'prev_source',
-        ['>'] = 'next_source',
-        ['i'] = 'show_file_details',
-      },
-    },
-    filesystem = {
-      filtered_items = {
-        visible = false,
-        hide_dotfiles = false,
-        hide_gitignored = false,
-        hide_hidden = true,
-      },
-      follow_current_file = {
-        enabled = true,
-        leave_dirs_open = false,
-      },
-      group_empty_dirs = false,
-      hijack_netrw_behavior = 'open_default',
-      use_libuv_file_watcher = true,
-      window = {
-        mappings = {
-          ['<bs>'] = 'navigate_up',
-          ['.'] = 'set_root',
-          ['H'] = 'toggle_hidden',
-          ['/'] = 'fuzzy_finder',
-          ['D'] = 'fuzzy_finder_directory',
-          ['#'] = 'fuzzy_sorter',
-          ['f'] = 'filter_on_submit',
-          ['<c-x>'] = 'clear_filter',
-          ['[g'] = 'prev_git_modified',
-          [']g'] = 'next_git_modified',
-          ['o'] = { 'show_help', nowait = false, config = { title = 'Order by', prefix_key = 'o' } },
-          ['oc'] = { 'order_by_created', nowait = false },
-          ['od'] = { 'order_by_diagnostics', nowait = false },
-          ['og'] = { 'order_by_git_status', nowait = false },
-          ['om'] = { 'order_by_modified', nowait = false },
-          ['on'] = { 'order_by_name', nowait = false },
-          ['os'] = { 'order_by_size', nowait = false },
-          ['ot'] = { 'order_by_type', nowait = false },
-        },
-        fuzzy_finder_mappings = {
-          ['<down>'] = 'move_cursor_down',
-          ['<C-n>'] = 'move_cursor_down',
-          ['<up>'] = 'move_cursor_up',
-          ['<C-p>'] = 'move_cursor_up',
-        },
-      },
+    lazy = false, -- neo-tree will lazily load itself
+    keys = {
+      { '<leader>e', '<cmd>Neotree toggle<cr>', desc = 'Toggle Neo-tree' },
     },
   },
 }
