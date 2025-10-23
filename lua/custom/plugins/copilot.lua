7224c0bfdd2381c0a4487fca43182cccb3813154
Truls Borgvall <truls.borgvall@icloud.com>
2025-10-23 13:34:41 +0200

    feat: use ctrl y to accept auto completion

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -3,4 +3,15 @@
 
 return {
   'github/copilot.vim',
+  config = function()
+    -- Disable default Tab mapping
+    vim.g.copilot_no_tab_map = true
+
+    -- Map Ctrl+y to accept suggestion (vim-like)
+    vim.keymap.set('i', '<C-y>', 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+      desc = 'Accept Copilot suggestion'
+    })
+  end,
 }
