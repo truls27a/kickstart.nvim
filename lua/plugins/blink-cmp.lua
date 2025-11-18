db83ef47df414b799a3c4d202f41191615654781
Truls Borgvall <truls.borgvall@icloud.com>
2025-11-18 16:31:58 +0100

    fix: update blink confg

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,32 +1,57 @@
 return {
   'saghen/blink.cmp',
-  version = '*',
+  event = 'VimEnter',
+  version = '1.*',
   dependencies = {
-    'L3MON4D3/LuaSnip',
-    'rafamadriz/friendly-snippets',
+    -- Snippet Engine
+    {
+      'L3MON4D3/LuaSnip',
+      version = '2.*',
+      build = (function()
+        -- Build Step is needed for regex support in snippets.
+        -- This step is not supported in many windows environments.
+        -- Remove the below condition to re-enable on windows.
+        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+          return
+        end
+        return 'make install_jsregexp'
+      end)(),
+      dependencies = {
+        -- {
+        --   'rafamadriz/friendly-snippets',
+        --   config = function()
+        --     require('luasnip.loaders.from_vscode').lazy_load()
+        --   end,
+        -- },
+      },
+      opts = {},
+    },
+    'folke/lazydev.nvim',
   },
   opts = {
-    keymap = { preset = 'default' },
+    keymap = {
+      preset = 'default',
+    },
+
     appearance = {
-      use_nvim_cmp_as_default = true,
       nerd_font_variant = 'mono',
     },
+
     completion = {
-      documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 200,
-      },
+      documentation = { auto_show = false, auto_show_delay_ms = 500 },
     },
+
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
+      default = { 'lsp', 'path', 'snippets', 'lazydev' },
       providers = {
-        lazydev = {
-          name = 'LazyDev',
-          module = 'lazydev.integrations.blink',
-          fallbacks = { 'lsp' },
-        },
+        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
       },
     },
+
+    snippets = { preset = 'luasnip' },
+
+    fuzzy = { implementation = 'lua' },
+
     signature = { enabled = true },
   },
 }
