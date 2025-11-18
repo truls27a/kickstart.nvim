89f075c0f48341aa2aa7535513473ad94d0d4bba
Truls Borgvall <truls.borgvall@icloud.com>
2025-11-18 16:15:00 +0100

    fix: improve diagnostic icons

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -2,28 +2,29 @@
 
 -- Configure diagnostic display
 vim.diagnostic.config {
-  virtual_text = {
-    prefix = function(diagnostic)
-      local icons = {
-        [vim.diagnostic.severity.ERROR] = '',
-        [vim.diagnostic.severity.WARN] = '',
-        [vim.diagnostic.severity.INFO] = '',
-        [vim.diagnostic.severity.HINT] = '',
-      }
-      return icons[diagnostic.severity] or '●'
-    end,
-    spacing = 4,
-    source = 'if_many',
-  },
-  signs = true,
-  underline = {
-    severity = { min = vim.diagnostic.severity.ERROR },
-  },
-  update_in_insert = false,
   severity_sort = true,
-  float = {
-    border = 'rounded',
+  float = { border = 'rounded', source = 'if_many' },
+  underline = { severity = vim.diagnostic.severity.ERROR },
+  signs = vim.g.have_nerd_font and {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '󰅚 ',
+      [vim.diagnostic.severity.WARN] = '󰀪 ',
+      [vim.diagnostic.severity.INFO] = '󰋽 ',
+      [vim.diagnostic.severity.HINT] = '󰌶 ',
+    },
+  } or {},
+  virtual_text = {
     source = 'if_many',
+    spacing = 2,
+    format = function(diagnostic)
+      local diagnostic_message = {
+        [vim.diagnostic.severity.ERROR] = diagnostic.message,
+        [vim.diagnostic.severity.WARN] = diagnostic.message,
+        [vim.diagnostic.severity.INFO] = diagnostic.message,
+        [vim.diagnostic.severity.HINT] = diagnostic.message,
+      }
+      return diagnostic_message[diagnostic.severity]
+    end,
   },
 }
 
