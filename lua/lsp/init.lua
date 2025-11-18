b322463a48d5baa230461a06d0d4b09b295fecd4
Truls Borgvall <truls.borgvall@icloud.com>
2025-11-18 15:41:37 +0100

    fix: use lsp 0.11+ way

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,9 +1,7 @@
--- LSP framework setup with on_attach and capabilities
-
-local M = {}
+-- LSP framework setup with LspAttach autocmd and capabilities
 
 -- Configure diagnostic display
-vim.diagnostic.config({
+vim.diagnostic.config {
   virtual_text = {
     prefix = function(diagnostic)
       local icons = {
@@ -27,60 +25,64 @@ vim.diagnostic.config({
     border = 'rounded',
     source = 'if_many',
   },
+}
+
+-- Set default LSP capabilities for all servers
+local capabilities = require('blink.cmp').get_lsp_capabilities()
+vim.lsp.config('*', {
+  capabilities = capabilities,
 })
 
--- Enhanced capabilities with blink.cmp
-M.capabilities = vim.lsp.protocol.make_client_capabilities()
-M.capabilities = require('blink.cmp').get_lsp_capabilities(M.capabilities)
+-- LspAttach autocmd - runs when any LSP server attaches to a buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+  callback = function(event)
+    local map = function(keys, func, desc, mode)
+      mode = mode or 'n'
+      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+    end
 
--- On attach function (called when LSP attaches to buffer)
-M.on_attach = function(client, bufnr)
-  local function map(keys, func, desc, mode)
-    mode = mode or 'n'
-    vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = 'LSP: ' .. desc })
-  end
+    -- Keymaps
+    map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+    map('gra', vim.lsp.buf.code_action, 'Code [A]ction', { 'n', 'x' })
+    map('grr', require('telescope.builtin').lsp_references, '[R]eferences')
+    map('gri', require('telescope.builtin').lsp_implementations, '[I]mplementations')
+    map('grd', require('telescope.builtin').lsp_definitions, '[D]efinition')
+    map('grD', vim.lsp.buf.declaration, '[D]eclaration')
+    map('grt', require('telescope.builtin').lsp_type_definitions, '[T]ype definition')
+    map('gO', require('telescope.builtin').lsp_document_symbols, 'D[O]cument symbols')
+    map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace symbols')
 
-  -- Keymaps
-  map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
-  map('gra', vim.lsp.buf.code_action, 'Code [A]ction', { 'n', 'x' })
-  map('grr', require('telescope.builtin').lsp_references, '[R]eferences')
-  map('gri', require('telescope.builtin').lsp_implementations, '[I]mplementations')
-  map('grd', require('telescope.builtin').lsp_definitions, '[D]efinition')
-  map('grD', vim.lsp.buf.declaration, '[D]eclaration')
-  map('grt', require('telescope.builtin').lsp_type_definitions, '[T]ype definition')
-  map('gO', require('telescope.builtin').lsp_document_symbols, 'D[O]cument symbols')
-  map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace symbols')
+    -- Toggle inlay hints (if supported)
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+      map('<leader>th', function()
+        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+      end, '[T]oggle Inlay [H]ints')
+    end
 
-  -- Toggle inlay hints (if supported)
-  if client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-    map('<leader>th', function()
-      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }))
-    end, '[T]oggle Inlay [H]ints')
-  end
+    -- Document highlighting
+    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+      local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
+      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.document_highlight,
+      })
 
-  -- Document highlighting
-  if client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-    local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
-    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-      buffer = bufnr,
-      group = highlight_augroup,
-      callback = vim.lsp.buf.document_highlight,
-    })
+      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.clear_references,
+      })
 
-    vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-      buffer = bufnr,
-      group = highlight_augroup,
-      callback = vim.lsp.buf.clear_references,
-    })
-
-    vim.api.nvim_create_autocmd('LspDetach', {
-      group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
-      callback = function(event)
-        vim.lsp.buf.clear_references()
-        vim.api.nvim_clear_autocmds({ group = 'lsp-highlight', buffer = event.buf })
-      end,
-    })
-  end
-end
-
-return M
+      vim.api.nvim_create_autocmd('LspDetach', {
+        group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
+        callback = function(event2)
+          vim.lsp.buf.clear_references()
+          vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
+        end,
+      })
+    end
+  end,
+})
--- lua/lsp/servers.lua
+++ lua/lsp/servers.lua
@@ -3,113 +3,43 @@
 local M = {}
 
 M.setup = function()
-  local lsp_config = require('lsp')
-
-  -- Setup mason-lspconfig
-  require('mason-lspconfig').setup({
-    handlers = {
-      -- Default handler for all servers
-      function(server_name)
-        require('lspconfig')[server_name].setup({
-          capabilities = lsp_config.capabilities,
-          on_attach = lsp_config.on_attach,
-        })
-      end,
-
-      -- Custom handlers for servers with specific settings
-      ['rust_analyzer'] = function()
-        local settings = require('lsp.settings.rust_analyzer')
-        require('lspconfig').rust_analyzer.setup({
-          capabilities = lsp_config.capabilities,
-          on_attach = lsp_config.on_attach,
-          settings = settings,
-        })
-      end,
-
-      ['ts_ls'] = function()
-        local settings = require('lsp.settings.ts_ls')
-        require('lspconfig').ts_ls.setup({
-          capabilities = lsp_config.capabilities,
-          on_attach = lsp_config.on_attach,
-          settings = settings,
-        })
-      end,
-
-      ['jsonls'] = function()
-        local settings = require('lsp.settings.jsonls')
-        require('lspconfig').jsonls.setup({
-          capabilities = lsp_config.capabilities,
-          on_attach = lsp_config.on_attach,
-          settings = settings,
-        })
-      end,
-
-      ['yamlls'] = function()
-        local settings = require('lsp.settings.yamlls')
-        require('lspconfig').yamlls.setup({
-          capabilities = lsp_config.capabilities,
-          on_attach = lsp_config.on_attach,
-          settings = settings,
-        })
-      end,
-
-      ['html'] = function()
-        local settings = require('lsp.settings.html')
-        require('lspconfig').html.setup({
-          capabilities = lsp_config.capabilities,
-          on_attach = lsp_config.on_attach,
-          filetypes = settings.filetypes,
-        })
-      end,
-
-      ['tailwindcss'] = function()
-        local settings = require('lsp.settings.tailwindcss')
-        require('lspconfig').tailwindcss.setup({
-          capabilities = lsp_config.capabilities,
-          on_attach = lsp_config.on_attach,
-          filetypes = settings.filetypes,
-        })
-      end,
-
-      ['eslint'] = function()
-        local settings = require('lsp.settings.eslint')
-        require('lspconfig').eslint.setup({
-          capabilities = lsp_config.capabilities,
-          on_attach = lsp_config.on_attach,
-          settings = settings,
-        })
-      end,
-
-      ['lua_ls'] = function()
-        local settings = require('lsp.settings.lua_ls')
-        require('lspconfig').lua_ls.setup({
-          capabilities = lsp_config.capabilities,
-          on_attach = lsp_config.on_attach,
-          settings = settings,
-        })
-      end,
-
-      ['basedpyright'] = function()
-        local settings = require('lsp.settings.basedpyright')
-        require('lspconfig').basedpyright.setup({
-          capabilities = lsp_config.capabilities,
-          on_attach = lsp_config.on_attach,
-          root_dir = settings.root_dir,
-          settings = settings.settings,
-        })
-      end,
-
-      ['sourcekit'] = function()
-        local settings = require('lsp.settings.sourcekit')
-        require('lspconfig').sourcekit.setup({
-          capabilities = lsp_config.capabilities,
-          on_attach = lsp_config.on_attach,
-          cmd = settings.cmd,
-          root_dir = settings.root_dir,
-        })
-      end,
+  -- Setup mason-lspconfig with ensure_installed
+  require('mason-lspconfig').setup {
+    ensure_installed = {
+      'rust_analyzer',
+      'ts_ls',
+      'bashls',
+      'jsonls',
+      'yamlls',
+      'marksman',
+      'html',
+      'cssls',
+      'tailwindcss',
+      'eslint',
+      'lua_ls',
+      'taplo',
+      'basedpyright',
     },
-  })
+    automatic_installation = false,
+  }
+
+  -- Enable LSP servers (Neovim 0.11+ way)
+  vim.lsp.enable {
+    'rust_analyzer',
+    'ts_ls',
+    'bashls',
+    'jsonls',
+    'yamlls',
+    'marksman',
+    'html',
+    'cssls',
+    'tailwindcss',
+    'eslint',
+    'lua_ls',
+    'taplo',
+    'basedpyright',
+    'sourcekit',
+  }
 end
 
 return M
