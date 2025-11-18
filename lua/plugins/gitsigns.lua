5febb206004619725f31b0c66c36c894c5531492
Truls Borgvall <truls.borgvall@icloud.com>
2025-11-18 16:08:43 +0100

    feat: reset git signs back to defaults

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,15 +1,8 @@
 return {
   'lewis6991/gitsigns.nvim',
   opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
     on_attach = function(bufnr)
-      local gitsigns = require('gitsigns')
+      local gitsigns = require 'gitsigns'
 
       local function map(mode, l, r, opts)
         opts = opts or {}
@@ -20,17 +13,17 @@ return {
       -- Navigation
       map('n', ']c', function()
         if vim.wo.diff then
-          vim.cmd.normal({ ']c', bang = true })
+          vim.cmd.normal { ']c', bang = true }
         else
-          gitsigns.nav_hunk('next')
+          gitsigns.nav_hunk 'next'
         end
       end, { desc = 'Jump to next git [c]hange' })
 
       map('n', '[c', function()
         if vim.wo.diff then
-          vim.cmd.normal({ '[c', bang = true })
+          vim.cmd.normal { '[c', bang = true }
         else
-          gitsigns.nav_hunk('prev')
+          gitsigns.nav_hunk 'prev'
         end
       end, { desc = 'Jump to previous git [c]hange' })
 
@@ -38,10 +31,10 @@ return {
       map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'Git [h]unk [s]tage' })
       map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'Git [h]unk [r]eset' })
       map('v', '<leader>hs', function()
-        gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+        gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
       end, { desc = 'Git [h]unk [s]tage (visual)' })
       map('v', '<leader>hr', function()
-        gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+        gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
       end, { desc = 'Git [h]unk [r]eset (visual)' })
       map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'Git [h]unk [S]tage buffer' })
       map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'Git [h]unk [u]ndo stage' })
@@ -50,7 +43,7 @@ return {
       map('n', '<leader>hb', gitsigns.blame_line, { desc = 'Git [h]unk [b]lame line' })
       map('n', '<leader>hd', gitsigns.diffthis, { desc = 'Git [h]unk [d]iff against index' })
       map('n', '<leader>hD', function()
-        gitsigns.diffthis('@')
+        gitsigns.diffthis '@'
       end, { desc = 'Git [h]unk [D]iff against last commit' })
 
       -- Toggles
