c24f85ee5be9c38f361eb89204562468e8f30387
Truls Borgvall <truls.borgvall@icloud.com>
2025-11-19 10:39:56 +0100

    fix: dynamicaly get root dir

--- lua/lsp/settings/basedpyright.lua
+++ lua/lsp/settings/basedpyright.lua
@@ -1,7 +1,19 @@
 local lspconfig = require('lspconfig')
 
 return {
-  root_dir = lspconfig.util.root_pattern('/Users/trulsborgvall/Documents/Skole/Bacheloroppgave/RealCube-server'),
+  root_dir = function(fname)
+    -- First try git root, then Python project markers, finally cwd
+    return vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
+      or lspconfig.util.root_pattern(
+        'pyproject.toml',
+        'setup.py',
+        'setup.cfg',
+        'requirements.txt',
+        'Pipfile',
+        'pyrightconfig.json'
+      )(fname)
+      or vim.fn.getcwd()
+  end,
   settings = {
     basedpyright = {
       analysis = {
