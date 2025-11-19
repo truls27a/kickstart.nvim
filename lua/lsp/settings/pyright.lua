fdeb132f035580a3d2b14214522fc48e74567e50
Truls Borgvall <truls.borgvall@icloud.com>
2025-11-19 12:56:20 +0100

    fix: remove local pyright usage implimentation

--- lua/lsp/settings/pyright.lua
+++ lua/lsp/settings/pyright.lua
@@ -1,35 +1,14 @@
-local lspconfig = require('lspconfig')
-
--- Helper function to find project-local pyright
-local function get_pyright_cmd(root_dir)
-  if root_dir then
-    local local_bin = root_dir .. '/node_modules/.bin/pyright-langserver'
-    if vim.fn.executable(local_bin) == 1 then
-      return { local_bin, '--stdio' }
-    end
-  end
-  return { 'pyright-langserver', '--stdio' }
-end
+local lspconfig = require 'lspconfig'
 
 return {
-  on_new_config = function(new_config, new_root_dir)
-    new_config.cmd = get_pyright_cmd(new_root_dir)
-  end,
   root_dir = function(fname)
     -- First try git root, then Python project markers, finally cwd
     return vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
-      or lspconfig.util.root_pattern(
-        'pyproject.toml',
-        'setup.py',
-        'setup.cfg',
-        'requirements.txt',
-        'Pipfile',
-        'pyrightconfig.json'
-      )(fname)
+      or lspconfig.util.root_pattern('pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', 'Pipfile', 'pyrightconfig.json')(fname)
       or vim.fn.getcwd()
   end,
   settings = {
-    python = {
+    basedpyright = {
       analysis = {
         typeCheckingMode = 'basic',
         autoSearchPaths = true,
